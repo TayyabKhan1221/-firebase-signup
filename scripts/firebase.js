b65edc73da78dcb98cf2d34f2afe3439e381942c
TayyabKhan1221 <Tayyabkhano603@gmail.com>
2024-07-26 23:13:12 +0500

    add file

---
+++ scripts/firebase.js
@@ -0,0 +1,28 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.4/firebase-app.js";
+
+import {
+    getAuth,
+    createUserWithEmailAndPassword,
+    signInWithEmailAndPassword,
+  } from "https://www.gstatic.com/firebasejs/10.12.4/firebase-auth.js";
+  
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCHTIdX6fiTrbGY827mUksrJBaSblpYMCg",
+  authDomain: "signup-login-2023d.firebaseapp.com",
+  projectId: "signup-login-2023d",
+  storageBucket: "signup-login-2023d.appspot.com",
+  messagingSenderId: "836315492063",
+  appId: "1:836315492063:web:1854efc840fc7b69a0436a"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+
+// Initialize Firebase Authentication and get a reference to the service
+  export const auth = getAuth(app);
+
+  export { auth, createUserWithEmailAndPassword, signInWithEmailAndPassword };
